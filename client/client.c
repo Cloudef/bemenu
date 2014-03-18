ff314f3c5e1b30fed112adb97ab5d29f5a2e108b
Jari Vetoniemi <mailroxas@gmail.com>
2014-03-18 19:35:10 +0200

    Add project skeleton

---
+++ client/client.c
@@ -0,0 +1,30 @@
+/**
+ * @file client.c
+ *
+ * Sample client using the libbemenu.
+ * Also usable as dmenu replacement.
+ */
+
+#include <stdlib.h>
+
+/**
+ * Main method
+ *
+ * This function gives and takes the life of our program.
+ *
+ * @param argc Number of arguments from command line
+ * @param argv Pointer to array of the arguments
+ * @return exit status of the program
+ */
+int main(int argc, char **argv)
+{
+    (void)argc, (void)argv;
+
+    /*
+     * code goes here
+     */
+
+    return EXIT_SUCCESS;
+}
+
+/* vim: set ts=8 sw=4 tw=0 :*/
---
+++ lib/draw/curses.c
@@ -0,0 +1,9 @@
+/**
+ * @file curses.c
+ */
+
+/*
+ * code goes here
+ */
+
+/* vim: set ts=8 sw=4 tw=0 :*/
