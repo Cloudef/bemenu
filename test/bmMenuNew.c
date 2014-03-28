17250cfd3f86784bbee2130b8ba3341ded2346b0
Jari Vetoniemi <mailroxas@gmail.com>
2014-03-28 22:00:23 +0200

    Add tests.

---
+++ test/bmMenuNew.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <bemenu.h>
+#include <assert.h>
+
+int main(int argc, char **argv)
+{
+    (void)argc, (void)argv;
+
+    // TEST: Instance bmMenu with all possible draw modes.
+    {
+        bmDrawMode i;
+        for (i = 0; i < BM_DRAW_MODE_LAST; ++i) {
+            bmMenu *menu = bmMenuNew(BM_DRAW_MODE_NONE);
+            assert(menu != NULL);
+            bmMenuRender(menu);
+            bmMenuFree(menu);
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/* vim: set ts=8 sw=4 tw=0 :*/
