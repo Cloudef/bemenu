312a606d54455e5d6549906275710605c6784a1e
Tobias Kortkamp <t@tobik.me>
2019-04-23 11:41:03 +0200

    Use epoll_create1() instead of epoll_create()
    epoll_create() takes a size argument not a flag like EPOLL_CLOEXEC [1].
    
    [1] https://linux.die.net/man/2/epoll_create
    
    Signed-off-by: Tobias Kortkamp <t@tobik.me>

--- lib/renderers/wayland/wayland.c
+++ lib/renderers/wayland/wayland.c
@@ -286,7 +286,7 @@ constructor(struct bm_menu *menu)
         wl_list_insert(&wayland->windows, &window->link);
     }
 
-    if (!efd && (efd = epoll_create(EPOLL_CLOEXEC)) < 0)
+    if (!efd && (efd = epoll_create1(EPOLL_CLOEXEC)) < 0)
         goto fail;
 
     struct epoll_event ep;
