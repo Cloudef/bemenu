338b0637408f6f6e3c4d3082a6fadaa6929b496c
wintermute <kjulius@gmx.de>
2024-11-20 02:47:40 +0100

    Feat: fuzzy matching flag -z
    Adds the option to use fuzzy matching when filtering menu items.
    Port of
    https://tools.suckless.org/dmenu/patches/fuzzymatch/dmenu-fuzzymatch-4.6.diff

--- client/common/common.h
+++ client/common/common.h
@@ -46,6 +46,7 @@ struct client {
     enum bm_password_mode password;
     enum bm_key_binding key_binding;
     char *monitor_name;
+    bool fuzzy;
 };
 
 char* cstrcopy(const char *str, size_t size);
